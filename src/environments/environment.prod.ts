1558b2e4a6fc072ddde70d1cfd29042a8c2bc6b5
Amit <amitmoryossef@gmail.com>
2020-07-07 12:41:37 +0300

    chore(firebase): add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDG-av_fhucDCL_cQv0z45q3g1u2lCJ2QA',
+    authDomain: 'sign-language-detector.firebaseapp.com',
+    databaseURL: 'https://sign-language-detector.firebaseio.com',
+    projectId: 'sign-language-detector',
+    storageBucket: 'sign-language-detector.appspot.com',
+    messagingSenderId: '1012541058868',
+    appId: '1:1012541058868:web:e25196b4c789d52eebff61',
+    measurementId: 'G-ES4X804B85'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDG-av_fhucDCL_cQv0z45q3g1u2lCJ2QA',
+    authDomain: 'sign-language-detector.firebaseapp.com',
+    databaseURL: 'https://sign-language-detector.firebaseio.com',
+    projectId: 'sign-language-detector',
+    storageBucket: 'sign-language-detector.appspot.com',
+    messagingSenderId: '1012541058868',
+    appId: '1:1012541058868:web:e25196b4c789d52eebff61',
+    measurementId: 'G-ES4X804B85'
+  }
 };
 
 /*
